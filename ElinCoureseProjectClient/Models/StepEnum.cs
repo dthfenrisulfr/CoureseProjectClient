bc11003613c52cebf434fe8fb6d34e0a271d501e
Станислав <baskervilo88@gmail.com>
2020-05-30 13:42:39 +0600

    Modals name fix

---
+++ ElinCoureseProjectClient/Models/StepEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElinCoureseProjectClient.Models
+{
+  public enum StepEnum
+  {
+    getCustomer = 0,
+    setCustomerTrue = 1,
+    setCustomerFalse = 2,
+    customerReadyTrue = 3,
+    customerReadyFalse = 4,
+    getProducts = 5,
+    getStaff = 6,
+    getOrder = 7
+  }
+}
